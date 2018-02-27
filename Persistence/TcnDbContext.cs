6dc3ed203cdc18b9c441cae8ddda9d45d6b4cbf3
jkmco <jackcakal@gmail.com>
2018-02-28 00:22:07 +0800

    Add SoftDelete, CreatedAt and LastUpdatedAt

--- Persistence/TcnDbContext.cs
+++ Persistence/TcnDbContext.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using TCN.Models;
 
@@ -16,6 +19,13 @@ namespace TCN.Persistence
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // shadow properties
+            modelBuilder.Entity<Transaction>().Property<bool>("IsDeleted");
+            modelBuilder.Entity<Transaction>().Property<DateTime>("DeletedAt");
+            modelBuilder.Entity<Transaction>().Property<DateTime>("CreatedAt");
+            modelBuilder.Entity<Transaction>().Property<DateTime>("LastUpdatedAt");
+
+            // fluent api
             modelBuilder.Entity<User>(eu => {
                 eu.ToTable("Users");
                 eu.Property(u => u.Name).HasColumnType("varchar(255)").IsRequired();
@@ -36,7 +46,43 @@ namespace TCN.Persistence
              modelBuilder.Entity<TransactionFx>(ef => {
                 ef.ToTable("TransactionFxs");
                 ef.Property(f => f.Name).HasColumnType("varchar(3)").IsRequired();
-            });
+            });   
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            OnBeforeSaving();
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            OnBeforeSaving();
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+        
+        private void OnBeforeSaving()
+        {
+            foreach (var entry in ChangeTracker.Entries<Transaction>())
+            {
+                var now = DateTime.UtcNow;
+                switch (entry.State)
+                {
+                    case EntityState.Modified:
+                        entry.CurrentValues["LastUpdatedAt"] = now;
+                        break;
+                    
+                    case EntityState.Added:
+                        entry.CurrentValues["IsDeleted"] = false;
+                        entry.CurrentValues["CreatedAt"] = now;
+                        entry.CurrentValues["LastUpdatedAt"] = now;
+                        break;
+
+                    case EntityState.Deleted:
+                        entry.State = EntityState.Modified;
+                        entry.CurrentValues["IsDeleted"] = true;
+                        break;
+                }
+            }
         }
     }
 }
